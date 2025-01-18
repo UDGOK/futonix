a702dce9a4dceb9442604cf1e1f58f8d91e839d2
Yasir <yasir@futonix.com>
2025-01-17 21:30:52 -0600

    fixed email errors

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,13 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_USER: process.env.SMTP_USER,
+    SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+    SMTP_TO_EMAIL: process.env.SMTP_TO_EMAIL,
+  },
 };
 
 export default nextConfig;
